b78f927a7ece8b3b776ba6d82a0cfe7648ac3124
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2023-05-16 15:17:08 +0400

    Hello derivation works
    Signed-off-by: Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>

--- modules/containers/docker.nix
+++ modules/containers/docker.nix
@@ -1,8 +1,26 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {pkgs, ...}: {
+  # inherit nixpkgs;
   environment.systemPackages = with pkgs; [
     # For docker:
     docker
+
+
+
+  # stdenv.mkDerivation rec {
+  #   name = "hello-2.9";
+  #   src = fetchurl {
+  #   url = "mirror://gnu/hello/${name}.tar.gz";
+  #     #  sha256 = "0wqd8sjmxfskrflaxywc7gqw7sfawrfvdxd9skxawzfgyy0pzdz6";
+  #   };
+  #   doCheck = true;
+
+  #   passthru.tests.version =
+  #   testVersion { package = hello; };
+  # }
+      (pkgsStatic.callPackage ./nix-docker/default.nix {})
   ];
+
+
 }
