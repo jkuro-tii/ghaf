7c61a3aab0c2aa2579559ce2423c18ef12814cec
Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>
2023-05-25 17:29:46 +0400

    Legacy files removed
    Signed-off-by: Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>

--- modules/development/packages.nix
+++ modules/development/packages.nix
@@ -1,29 +1,7 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs;
-/*
-  let dockerScript = 
-    pkgs.writeScriptBin "run-docker-build" ''
-      #! {native.stdenv.shell}
-      set -e
-
-      echo "building root image..." >&2
-      imageOut=$(nix-build -A image --no-out-link)
-      echo "importing root image..." >&2
-      docker load < $imageOut
-      echo "building {unstable.version}..." >&2
-      cp -f {baseDocker} Dockerfile
-      docker build -t lnl7/nix:{unstable.version} .
-      docker rmi nix-base:{unstable.version}
-      ''
-    ; 
-  in*/
-  let 
-    jk_ =     (pkgs.callPackage ./docker.nix { inherit pkgs; });
-    jk = builtins.trace (">>>> package = " + jk_) jk_;
-  in
-  [
+  environment.systemPackages = with pkgs; [
     # For lspci:
     pciutils
 
@@ -40,35 +18,5 @@
 
     traceroute
     dig
-
-    # jk
-#    (pkgs.callPackage ./docker.nix { inherit pkgs; })
-
-    # (pkgs.stdenv.mkDerivation {
-    #   name = builtins.trace ">>>>docker_test";
-    #   ph
-    # ases = [ "installPhase" ];
-    # #   src = pkgs.lib.cleanSource ./.;
-    #   installPhase = /*builtins.trace ("out=" + out)*/ ''
-    #     echo "out= " $out
-    #     mkdir -p $out/dockerdata
-    #     touch $out/jk.test
-    #     echo cp ${dockerScript}/run-docker-build $out
-    #     ''
-    #     ;
-    # })
-  # (pkgs.dockerTools.buildImage {
-  #   name = "hello";
-  #   tag = "latest";
-  #   created = "now";
-  #   copyToRoot = pkgs.buildEnv {
-  #     name = "image-root";
-  #     paths = [ pkgs.hello ];
-  #     pathsToLink = [ "/bin" ];
-  #   };
-
-  #   config.Cmd = [ "/bin/hello" ];
-  # })
-
   ];
 }
