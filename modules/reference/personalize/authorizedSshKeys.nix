c7af56f86fee8fca51a82253931d648e9a88c1b2
Ganga Ram <Ganga.Ram@tii.ae>
2024-10-02 10:34:48 +0400

    Shared memory refactoring. Now gui sent via shm.
    Signed-off-by: Jaroslaw Kurowski <jaroslaw.kurowski@tii.ae>

--- modules/reference/personalize/authorizedSshKeys.nix
+++ modules/reference/personalize/authorizedSshKeys.nix
@@ -28,7 +28,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfyjcPGIRHEtXZgoF7wImA5gEY6ytIfkBeipz4lwnj6 Ganga.Ram@tii.ae"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsRi+yikYJjpLhJZHlCb9N4Wb7fHUEh0Y6XekD9DyTQ juanpablo.ruiz@unikie.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEA7p7hHPvPT6uTU44Nb/p9/DT9mOi8mpqNllnpfawDE tanel@nixos"
-
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwGPH/oOrD1g15uiPV4gBKGk7f8ZBSyMEaptKOVs3NG jaroslawkurowski@TII-JaroslawKurowski"
     # For ghaf-installer automated testing:
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAolaKCuIUBQSBFGFZI1taNX+JTAr8edqUts7A6k2Kv7"
   ];
